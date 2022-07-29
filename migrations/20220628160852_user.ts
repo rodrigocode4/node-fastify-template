28f1b13073bdbc77eefd52af0587393ee2188061
Rodrigo Oliveira <rodrigocode4@gmail.com>
2022-07-29 10:44:06 -0300

    chore: add pre-commit hook to husky

--- migrations/20220628160852_user.ts
+++ migrations/20220628160852_user.ts
@@ -1,6 +1,5 @@
 import { Knex } from 'knex'
 
-
 export async function up(knex: Knex): Promise<void> {
   return knex.schema
     .createTable('users', (table) => {
@@ -11,9 +10,7 @@ export async function up(knex: Knex): Promise<void> {
     })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
   return knex.schema
     .dropTable('users')
 }
-
